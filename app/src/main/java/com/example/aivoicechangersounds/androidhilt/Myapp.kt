d4ba03371d8ca9cf767319fb3e3b0860053f0386
Muhammad-muaz-dev <muhammadmuaz.dev@gmail.com>
2026-04-23 18:20:55 +0500

    Changed Flow execution in progress

--- app/src/main/java/com/example/aivoicechangersounds/androidhilt/Myapp.kt
+++ app/src/main/java/com/example/aivoicechangersounds/androidhilt/Myapp.kt
@@ -12,7 +12,7 @@ class Myapp: Application() {
         // TODO: Replace with real login flow later
         // For now set the token once on app start for testing
         TokenProvider.setToken(
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMSIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE3NzY5NDQ0MjEsImV4cCI6MTc3Njk0NjIyMX0.xfT4oKK5Wqz5XiKVH9bpf7-XK5sgrUgKrmmh3IIAYyo"
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMSIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE3NzY5NDY4NzksImV4cCI6MTc3Njk0ODY3OX0.8G6AN-gqwi_QO2X2rMN2YqOOg4wYr0FHUONRYJDWcWc"
         )
     }
 }
